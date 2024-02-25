434e9f1df73f938fa86f8b08f5dc0e5518f40c4e
laura <l.restrepop@uniandes.edu.co>
2024-02-25 18:57:50 -0500

    Added functional clock

---
+++ Assets/Scripts/MinuteHandMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class MinuteHandMovement : MonoBehaviour
+{
+    public float totalRotationAngle = 360f; 
+    public float durationMinutes = 0.5f; 
+
+    private float rotationSpeed;
+    private float durationSeconds;
+    private float elapsedTime = 0f;
+
+    public Transform pivotPoint; 
+
+    private void Start()
+    {
+        durationSeconds = durationMinutes * 60;
+        rotationSpeed = totalRotationAngle / durationSeconds;
+    }
+
+    private void Update()
+    {
+
+        if (elapsedTime < durationSeconds)
+        {
+            transform.RotateAround(pivotPoint.position, Vector3.left, rotationSpeed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+        }
+        
+    }
+
+
+}
