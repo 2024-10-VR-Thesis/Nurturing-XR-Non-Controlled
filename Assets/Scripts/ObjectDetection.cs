c445ae3964d69b2a2928515841f4897c9be40a9e
BorisNRR <b.reyesr@uniandes.edu.co>
2024-05-22 14:54:59 -0500

    Adaptacion a nuevo API
    Co-Authored-By: Laura Restrepo <69609680+Laurarestrepo03@users.noreply.github.com>

--- Assets/Scripts/ObjectDetection.cs
+++ Assets/Scripts/ObjectDetection.cs
@@ -4,6 +4,9 @@ using System.Net.Http;
 using System.Collections;
 using TMPro;
 using UnityEngine.Networking;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 public class ObjectDetection : MonoBehaviour
 {
@@ -12,34 +15,16 @@ public class ObjectDetection : MonoBehaviour
     void Start()
     {
         // FIXME: each headset should define when to start, in separate file
-        StartCoroutine(DelayedCaptureAndSendScreenshot());
+        //StartCoroutine(DetectObjects());
     }
 
-    IEnumerator DelayedCaptureAndSendScreenshot()
+    IEnumerator DetectObjects()
     {
-        // Wait for 2 seconds
-        yield return new WaitForSeconds(2f);
-
-        // FIXME: this should be in Start()
-        StartCoroutine(CaptureAndSendScreenshot());
-    }
-
-    IEnumerator CaptureAndSendScreenshot()
-    {
-        string screenshotName = System.Guid.NewGuid().ToString();
-        string screenshotPath = Path.Combine(Application.dataPath, "ScreenCaptures", screenshotName);
-        string normalizedPath = screenshotPath.Replace("\\", "/");
-
-        string url = "http://127.0.0.1:5000/screenshot";
-        Debug.Log("This is the path: " + screenshotPath);
-        string jsonBody = "{\"screenshot_name\": \"" + normalizedPath + "\"}";
-        print(jsonBody);
-
+        string url = "http://127.0.0.1:5000/detect";
         UnityWebRequest request = new UnityWebRequest(url, "POST");
-        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonBody);
-        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
-        request.downloadHandler = new DownloadHandlerBuffer();
         request.SetRequestHeader("Content-Type", "application/json");
+        request.uploadHandler = new UploadHandlerRaw(null);
+        request.downloadHandler = new DownloadHandlerBuffer();
 
         yield return request.SendWebRequest();
 
@@ -47,49 +32,13 @@ public class ObjectDetection : MonoBehaviour
         {
             Debug.LogError("Error: " + request.error);
         }
-        /*
         else
         {
-            Debug.Log($"Screenshot request with name {normalizedPath} sent successfully");
+            string jsonResponse = request.downloadHandler.text;
+            List<string> responseList = JsonConvert.DeserializeObject<List<string>>(jsonResponse);
+            worldText.text = responseList[2];
         }
-        */
 
-        SendScreenshot(screenshotPath+".png");
     }
 
-    async void SendScreenshot(string screenshotPath)
-    {
-        string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZjg4ZWMyNDUtOGNhMi00MGE2LWE4NzEtMWM5ZmU0ZmE5YWNlIiwidHlwZSI6ImFwaV90b2tlbiJ9.BV9MP-DFCnUdMlrkkN9E-nWR_ThUmTlI11-eBlguDRM";
-        string url = "https://api.edenai.run/v2/image/object_detection";
-
-        MultipartFormDataContent form = new MultipartFormDataContent();
-        form.Add(new StringContent("amazon"), "providers");
-        form.Add(new StringContent("google"), "fallback_providers");
-
-        byte[] imageBytes = File.ReadAllBytes(screenshotPath);
-        ByteArrayContent imageContent = new ByteArrayContent(imageBytes);
-        form.Add(imageContent, "file", Path.GetFileName(screenshotPath));
-
-        HttpClient client = new HttpClient();
-        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
-
-        HttpResponseMessage response = await client.PostAsync(url, form);
-
-        if (response.IsSuccessStatusCode)
-        {
-            string responseBody = await response.Content.ReadAsStringAsync();
-            dynamic result = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
-
-            Debug.Log(result.amazon.items);
-            worldText.text = string.Join(", ", result.amazon.items);
-        }
-        else
-        {
-            Debug.LogError($"Error: {response.StatusCode} - {response.ReasonPhrase} - {response}");
-            //worldText.text = $"Error: {response.StatusCode} - {response.ReasonPhrase} - {response}";
-        }
-
-        // TODO: delete screenshots, but wait until game is finished, otherwise this wont load
-
-    }
 }
